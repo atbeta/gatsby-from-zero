358641a175039d466071b5d7c75af6990fda4511
Beta <me@pbeta.me>
2020-01-02 19:28:04 +0800

    第三部分完成

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,23 @@
 import React from "react"
-import { graphql } from "gatsby"
+import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
 
 const Index = ({ data }) => {
   return (
     <Layout>
       <div>
-        <h1>文章的列表</h1>
+        <h1>文章列表</h1>
         <h4>共{data.allMarkdownRemark.totalCount} 篇文章</h4>
         {data.allMarkdownRemark.edges.map(({ node }) => (
           <div key={node.id}>
+            <Link to={node.fields.slug}>
             <h3>
               {node.frontmatter.title}{" "}
               <span>
                 — {node.frontmatter.date}
               </span>
             </h3>
+            </Link>
             <p>{node.excerpt}</p>
           </div>
         ))}
@@ -36,6 +38,9 @@ export const query = graphql`
           frontmatter {
             title
             date(formatString: "DD MMMM, YYYY")
+          }
+          fields {
+            slug
           }
           excerpt
         }
