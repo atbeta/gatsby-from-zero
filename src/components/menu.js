d96da785eb40866f1c76d755bf36eb3bc676d228
Beta <me@pbeta.me>
2020-01-21 23:58:08 +0800

    修改顶部菜单样式

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,15 +1,26 @@
 
 import React from 'react'
 import PropTypes from 'prop-types'
+import { css } from "@emotion/core"
 import { NavLink } from '@theme-ui/components'
+
+const menuStyle = css`
+display: flex;
+justify-content: flex-end;
+font-size: 14px;
+.menu-item {
+  flex: 1;
+  margin-left: 20px;
+}
+`
 
 const Menu = ({ menuList }) => {
   const menu = menuList.map((item, index) => (
-    <NavLink href={item.path} p={2} key={index}>
+    <NavLink href={item.path} p={2} key={index} className="menu-item">
       {item.title}
     </NavLink>
   ))
-return <div>{menu}</div>
+return <div css={menuStyle}>{menu}</div>
 }
 
 Menu.propTypes = {
